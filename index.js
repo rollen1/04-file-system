d1c681bc772920d74f05cda561fdc2c6413901e2
rollen1 <nolovedivision@gmail.com>
2022-06-08 21:20:06 +0300

    update

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log('labas rytas')
+import { file } from './lib/file.js';
+
+const userMaryte = {
+    name: "tevs",
+    age: 87,
+    isMarried: false,
+}
+
+const createStatus = await file.create('asd', 'betkas.txt', userMaryte)
+
+const readStatus = await file.read('users', 'petras.json');
+console.log(readStatus);
+
+// console.log(file);
+
+// const user = {
+//     name: 'Petras',
+//     age: 99,
+//     isLoggedIn: false,
+// }
+
+// const status = file.create('users', 'petras.json', user);
+
+// const apiStatus = await fetch('');
+
+// if (apiStatus) 
